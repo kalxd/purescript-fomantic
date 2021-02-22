3bb437a785264c01db38b4e1af0749d7c0f2aa23
荀徒之 <kalxd@126.com>
2021-02-22 19:22:56 +0800

    简单的例子。

--- example/spago.dhall
+++ example/spago.dhall
@@ -3,7 +3,8 @@ Welcome to a Spago project!
 You can edit this file as you like.
 -}
 { name = "my-project"
-, dependencies = [ "concur-react", "console", "effect", "psci-support" ]
-, packages = ./packages.dhall
-, sources = [ "src/**/*.purs", "test/**/*.purs" ]
+, dependencies =
+  [ "aff", "concur-react", "console", "effect", "psci-support", "routing" ]
+, packages = ../packages.dhall
+, sources = [ "src/**/*.purs" ]
 }
--- example/src/Main.purs
+++ example/src/Main.purs
@@ -7,13 +7,59 @@ import Concur.React (HTML)
 import Concur.React.DOM as D
 import Concur.React.Props as P
 import Concur.React.Run (runWidgetInDom)
+import Data.Foldable (oneOf)
 import Effect (Effect)
+import Effect.Aff (Milliseconds(..), delay, launchAff_)
+import Effect.Class.Console (logShow)
+import Routing.Hash (matches)
+import Routing.Match (Match, root, lit, end)
 
-mybutton :: Int -> Widget HTML Int
-mybutton n = do
-  void $ D.button [P.className "ui primary button", P.onClick] [D.text $ show n]
-  mybutton $ n + 1
+-- | 我们的全局路由
+data AppR = HomeR
+          | ButtonR
+          | NotFoundR
+
+instance showAppR :: Show AppR where
+  show HomeR = "Home R"
+  show ButtonR = " Button R"
+  show NotFoundR = "NotFound R"
+
+homeR :: Match AppR
+homeR =  HomeR <$ end
+
+buttonR :: Match AppR
+buttonR = lit "button" $> ButtonR <* end
+
+router :: Match AppR
+router = root *> lit "" *> oneOf [ buttonR
+                       , homeR
+                       , pure NotFoundR
+                       ]
+
+homePage :: forall a. Widget HTML a
+homePage = D.div [P.className "ui container"] [D.text "hello"]
+
+buttonPage :: forall a. Widget HTML a
+buttonPage = D.div [P.className "ui container"] [button]
+  where button = D.button [P.className "ui primary button"] [D.text "hello"]
+
+notFoundPage :: forall a. Widget HTML a
+notFoundPage = D.h1 [] [D.text "什么都没有"]
+
+routerForPage :: forall a. AppR -> Widget HTML a
+routerForPage HomeR = homePage
+routerForPage ButtonR = buttonPage
+routerForPage NotFoundR = notFoundPage
+
+runApp :: forall a. Widget HTML a -> Effect Unit
+runApp = runWidgetInDom "app"
 
 main :: Effect Unit
 main = do
-  runWidgetInDom "app" $ mybutton 10
+  launchAff_ do
+    delay $ Milliseconds 0.0
+  join $ matches router \a router' -> do
+    logShow a
+    logShow router'
+    let app = routerForPage router'
+    runApp app
