2935837c206dbc4b8b7e1dd971f8c9c66d0f3fc2
荀徒之 <kalxd@126.com>
2021-02-23 14:59:52 +0800

    小界面。

--- example/src/Main.purs
+++ example/src/Main.purs
@@ -5,14 +5,14 @@ import Prelude
 import Concur.Core (Widget)
 import Concur.React (HTML)
 import Concur.React.DOM as D
-import Concur.React.Props as P
 import Concur.React.Run (runWidgetInDom)
 import Effect (Effect)
 
 import Concur.Fomantic as B
+import Concur.Fomantic.Container (container)
 
 homePage :: forall a. Widget HTML a
-homePage = D.div [P.className "ui container"] [B.uiButton [] [] [D.text "sb"]]
+homePage = container [B.uiButton [] [] [D.text "sb"]]
 
 runApp :: forall a. Widget HTML a -> Effect Unit
 runApp = runWidgetInDom "app"
