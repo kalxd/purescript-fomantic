ebe0520da4ff7e7a41f6381542164cd3baeba026
荀徒之 <kalxd@126.com>
2021-02-28 19:45:36 +0800

    点击之后还能知道当前哪个菜单响应了。

--- example/src/Main.purs
+++ example/src/Main.purs
@@ -7,21 +7,33 @@ import Concur.Fomantic.Internal.DOM (UIWidget)
 import Concur.React.DOM as D
 import Concur.React.Props (className, href)
 import Concur.React.Run (runWidgetInDom)
+import Control.Alt ((<|>))
 import Effect (Effect)
-import Page.Route (routeWidget)
+import Page.Route (AppRoute(..), routeWidget, watchRouteChange)
 
-topMenu :: forall a. UIWidget a
-topMenu = D.nav [className "ui menu"] [body]
-  where body = container menus
-        menus = [ D.a [className "item", href "#/"] [D.text "首页"]
-                , D.a [className "item", href "#/button"] [D.text "按钮"]
+topMenu :: forall a. AppRoute -> UIWidget a
+topMenu route = D.nav [className "ui menu"] [body]
+  where itemClass item = if route == (item :: AppRoute) then
+                           className "active item"
+                         else
+                           className "item"
+        body = container menus
+        menus = [ D.a [itemClass HomeR, href "#/"] [D.text "首页"]
+                , D.a [itemClass ButtonR, href "#/button"] [D.text "按钮"]
                 ]
+
+topMenuWidget :: forall a. UIWidget a
+topMenuWidget = watchRouteChange \widget -> do
+  go widget HomeR
+  where go awaitWidget route = do
+          route' <- awaitWidget <|> topMenu route
+          go awaitWidget route'
 
 runApp :: forall a. UIWidget a -> Effect Unit
 runApp = runWidgetInDom "app"
 
 appWidget :: forall a. UIWidget a
-appWidget = D.div' [ topMenu
+appWidget = D.div' [ topMenuWidget
                    , container [routeWidget]
                    ]
 
--- example/src/Page/Route.purs
+++ example/src/Page/Route.purs
@@ -5,18 +5,20 @@ import Prelude
 import Concur.Fomantic.Internal.DOM (UIWidget)
 import Control.Alt ((<|>))
 import Data.Foldable (oneOf)
+import Effect.AVar as EV
+import Effect.Aff (Milliseconds(..), delay)
+import Effect.Aff.AVar as AV
+import Effect.Aff.Class (liftAff)
 import Effect.Class (liftEffect)
 import Page.Button (buttonPage)
 import Page.Home (homePage)
 import Routing.Hash (matches)
 import Routing.Match (Match, end, lit, root)
-import Effect.AVar as EV
-import Effect.Aff (Milliseconds(..), delay)
-import Effect.Aff.AVar as AV
-import Effect.Aff.Class (liftAff)
 
 data AppRoute = HomeR
               | ButtonR
+
+derive instance eqAppRoute :: Eq AppRoute
 
 homeR :: Match AppRoute
 homeR = end $> HomeR
@@ -31,8 +33,8 @@ pickWidget :: forall a. AppRoute -> UIWidget a
 pickWidget HomeR = homePage
 pickWidget ButtonR = buttonPage
 
-routeWidget :: forall a. UIWidget a
-routeWidget = do
+watchRouteChange :: forall a. (UIWidget AppRoute -> UIWidget a) -> UIWidget a
+watchRouteChange action = do
   routeRef <- liftEffect do
     var <- EV.empty
     void $ matches appRoute \_ route -> do
@@ -40,7 +42,25 @@ routeWidget = do
     pure var
   let awaitRoute = liftAff $ AV.take routeRef
   liftAff $ delay $ Milliseconds 0.0
-  go awaitRoute HomeR
+  action awaitRoute
+
+-- routeWidget :: forall a. UIWidget a
+-- routeWidget = do
+--   routeRef <- liftEffect do
+--     var <- EV.empty
+--     void $ matches appRoute \_ route -> do
+--       void $ EV.tryPut route var
+--     pure var
+--   let awaitRoute = liftAff $ AV.take routeRef
+--   liftAff $ delay $ Milliseconds 0.0
+--   go awaitRoute HomeR
+--   where go awaitRoute route = do
+--           route' <- awaitRoute <|> pickWidget route
+--           go awaitRoute route'
+
+routeWidget :: forall a. UIWidget a
+routeWidget = watchRouteChange \route -> do
+  go route HomeR
   where go awaitRoute route = do
           route' <- awaitRoute <|> pickWidget route
           go awaitRoute route'
